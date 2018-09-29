ff3d928d5ae6ffcb4295661910f7c64b4e7c9f98
ZZRRegion <854974242@qq.com>
2018-09-29 23:30:36 +0800

    提交代码

---
+++ PhoneQuery/PhoneQuery/FrmMain.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PhoneQuery
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.btnQuery = new System.Windows.Forms.Button();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnQuery
+            // 
+            this.btnQuery.Location = new System.Drawing.Point(195, 56);
+            this.btnQuery.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.btnQuery.Name = "btnQuery";
+            this.btnQuery.Size = new System.Drawing.Size(87, 30);
+            this.btnQuery.TabIndex = 0;
+            this.btnQuery.Text = "查询";
+            this.btnQuery.UseVisualStyleBackColor = true;
+            this.btnQuery.Click += new System.EventHandler(this.btnQuery_Click);
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(20, 56);
+            this.txtPhone.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(165, 30);
+            this.txtPhone.TabIndex = 1;
+            this.txtPhone.Text = "18959296933";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(17, 16);
+            this.lblPhone.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(89, 20);
+            this.lblPhone.TabIndex = 2;
+            this.lblPhone.Text = "手机号：";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(20, 96);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(262, 245);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 352);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.btnQuery);
+            this.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "手机号归属地查询";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuery;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ PhoneQuery/PhoneQuery/FrmMain.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace PhoneQuery
+{
+    public partial class FrmMain : Form
+    {
+        private string cloudUrl => "http://m.ip138.com/mobile.asp?mobile={0}";
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuery_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Clear();
+            string url = string.Format(this.cloudUrl, this.txtPhone.Text);
+            HttpClient httpClient = new HttpClient();
+            string content = httpClient.GetStringAsync(url).Result;
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(content);
+            HtmlNode htmlNode = htmlDocument.DocumentNode.SelectSingleNode(".//table[@class='table']");
+            string text = htmlNode.InnerText;
+            text = text.Replace("\t", "");
+            string[] lst = text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 1; i < lst.Length; i++)
+            {
+                this.richTextBox1.AppendText(lst[i]);
+                this.richTextBox1.AppendText(Environment.NewLine);
+            }
+        }
+    }
+}
